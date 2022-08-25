e31bacc68424036e6b591f093f39523defe598fe
elena <kvolikhelen87@gmail.com>
2022-08-25 10:21:39 -0500

    payment class completed

--- src/Login/Payment.java
+++ src/Login/Payment.java
@@ -4,5 +4,7 @@ public class Payment {
     //payment created
     public static void main(String[] args) {
         //payment updated
+        System.out.println("hello");
+        //payment done
     }
 }
